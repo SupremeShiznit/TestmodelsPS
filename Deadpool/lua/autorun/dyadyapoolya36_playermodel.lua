b900a41bdc9293b65043a5d1318ca9b8a9670620
SupremeShiznit <tarbouriech.theo@gmail.com>
2014-04-12 23:23:40 +0200

    Deadpool change

---
+++ Deadpool/lua/autorun/dyadyapoolya36_playermodel.lua
@@ -0,0 +1,4 @@
+player_manager.AddValidModel( "dyadyapoolya36", "models/apocmodels/Deadpool/dyadyapoolya36.mdl" )
+player_manager.AddValidHands( "dyadyapoolya36", "models/weapons/c_arms_deadpool.mdl", 0, "00000000" )
+
+list.Set( "dyadyapoolya36", "models/apocmodels/Deadpool/dyadyapoolya36.mdl" )
